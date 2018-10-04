cdb92df3326f22b3326761d28b3fff77d6f11409
BT_1N3_12 <wleung3409@bths.edu>
2018-10-04 11:48:32 -0400

    Classroom is close to done.

--- src/com/company/Classroom.java
+++ src/com/company/Classroom.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+    private static String subject = "Math";
+    public Classroom(Student[] students, Teacher teacher){
+        super("Classroom");
+        this.students = students;
+        this.teacher = teacher;
+        this.subject = "Math";
+    }
+    public getSubject(){
+        return Classroom.subject;
+    }
+    public int classAverage(){
+        int num = 0;
+        int total = 0;
+        double avg = 0;
+        for(int i = 0; i < students.length; i++){
+            total += students[i].getGPA();
+            num++;
+        }
+        avg = total/num;
+    }
 }
