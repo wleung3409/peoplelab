895fde1ef5f791bcf2fc9a4c796f7b39e5bf3304
jleung1239 <jleung1239>
2018-10-04 11:43:14 -0400

    student

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class Student {
+public class Student extends Person{
+    private double GPA;
+    private String color;
+    private String subject;
+
+    public Student(double GPA, String color, String subject){
+        super("studentFirst", "studentLast");
+        this.GPA = GPA;
+        this.color = color;
+    }
+
+    public double getGPA(){
+        return GPA;
+    }
+    public double getColor;
+    public String toString(){
+
+    }
 }
